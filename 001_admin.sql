e3b8d366ad25a28869674fa0a6028c8e3ebdf0e7
sivakumarkatam <siva.katam@blazeclan.com>
2017-11-22 11:53:47 +0530

    Update 001_admin.sql

--- 001_admin.sql
+++ 001_admin.sql
@@ -15,17 +15,17 @@ DROP TABLE IF EXISTS APGADMIN_CCExpirySchedular_Settings;
 -- !40101 SET @saved_cs_client     = @@character_set_client 
 -- !40101 SET character_set_client = utf8
 CREATE TABLE `APGADMIN_CCExpirySchedular_Settings` (
- SettingId bigint(20) NOT NULL auto_increment,
- EmailTemplate text NOT NULL,
- SenderEmailId varchar(255) NOT NULL,
- EmailSubject text NOT NULL,
- NoOfMonthsBeforeExpiry int(11) NOT NULL,
- NoOfEmailsPerMonth int(11) NOT NULL,
- DefaultReceiverEmailId varchar(255) NOT NULL,
- ErrorEmailSubject text NOT NULL,
- ErrorEmailTemplate text NOT NULL,
- SuccessEmailTemplate text NOT NULL,
- SuccessEmailSubject text NOT NULL,
- ExpiryType varchar(10) NOT NULL,
- PRIMARY KEY (`SettingId`)
+ SettingId bigint(20) NOT NULL auto_increment
+ ,EmailTemplate text NOT NULL
+ ,SenderEmailId varchar(255) NOT NULL
+ ,EmailSubject text NOT NULL
+ ,NoOfMonthsBeforeExpiry int(11) NOT NULL
+ ,NoOfEmailsPerMonth int(11) NOT NULL
+ ,DefaultReceiverEmailId varchar(255) NOT NULL
+ ,ErrorEmailSubject text NOT NULL
+ ,ErrorEmailTemplate text NOT NULL
+ ,SuccessEmailTemplate text NOT NULL
+ ,SuccessEmailSubject text NOT NULL
+ ,ExpiryType varchar(10) NOT NULL
+ ,PRIMARY KEY (`SettingId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
