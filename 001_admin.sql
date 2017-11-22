76e1226bf25658c63525a6842d95adf07d77cc04
sivakumarkatam <siva.katam@blazeclan.com>
2017-11-22 11:15:29 +0530

    Update 001_admin.sql

--- 001_admin.sql
+++ 001_admin.sql
@@ -3,7 +3,7 @@
 -- Host: transactiondb.ch29bksppiii.ap-southeast-1.rds.amazonaws.com    Database: devAdminDB
 -- ------------------------------------------------------
 -- Server version	5.6.27-log
-
+--
 --/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
 --/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
@@ -14,11 +14,11 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */
-
+--
 --
 -- Table structure for table `APGADMIN_CCExpirySchedular_Settings`
 --
-
+--
 DROP TABLE IF EXISTS `APGADMIN_CCExpirySchedular_Settings`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
